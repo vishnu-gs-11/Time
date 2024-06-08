90368a5f865d6a8132ddfd8d5041e076b2b2e942
Vishnu Ganesan <115076749+vishnu-gs-11@users.noreply.github.com>
2024-06-08 15:19:17 +0530

    Create index.js

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+import "./index.css";
+
+const NotFound = () => {
+  return (
+    <div className="not-found-container">
+      <p>
+        Page Not Found
+      </p>
+      
+    </div>
+  );
+};
+
+export default NotFound;
