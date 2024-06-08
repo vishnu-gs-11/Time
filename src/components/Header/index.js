9ae7a0e255cac7cc434f7c1ead6cc6df818765b3
Vishnu Ganesan <115076749+vishnu-gs-11@users.noreply.github.com>
2024-06-08 15:18:00 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,30 @@
+import { Link } from "react-router-dom";
+import "./index.css";
 
+const Header = () => {
+  return (
+    <nav>
+      <div>
+      
+      <div>
+        <div>
+          <ul className="nav-list">
+            <li>
+              <Link to="/" className="nav-item">Home</Link>
+            </li>
+            <li>
+              <Link to="/counter" className="nav-item">Counter</Link>
+            </li>
+            <li>
+              <Link to="/timer" className="nav-item">Timer</Link>
+            </li>
+          </ul>
+        </div>
+        <hr></hr>
+      </div>
+    </div>
+    </nav>
+  );
+};
+
+export default Header;
